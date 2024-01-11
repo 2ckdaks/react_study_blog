92a63791adff15a34ce9463fbbfa4cab22072bd2
changmin <changmin@digital-x.kr>
2024-01-11 17:12:47 +0900

    JSX 문법 학습 데이터바인딩

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let post = '강남 우동 맛집';
+
   return (
     <div className='App'>
       <div className='black-nav'>
         <h4>WelCome to Chnagmin's BLOG 👋</h4>
       </div>
+
+      <div>블로그임</div>
+      <div>{post}</div>
     </div>
   );
 }
